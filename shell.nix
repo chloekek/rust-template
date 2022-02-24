03c9cf64a8a9cd5eed17795fbd951072055d5399
Chloé Kekoa <chloekek@use.startmail.com>
2022-02-25 00:11:33 +0100

    Update Rust

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@
 
 let
     rust = nixpkgs.rustChannelOf {
-        date = "2022-01-20";
+        date = "2022-02-24";
         channel = "nightly";
-        sha256 = "153vg6nmiya6pykqnn9r7dzzl1cq77l7kri7c2k8v78mi69hqbwq";
+        sha256 = "13sawwb4yn3akj68s59ba1hrqi4lr59ihdis0mvbbcq4p934m4jf";
     };
 in
     nixpkgs.mkShell {
