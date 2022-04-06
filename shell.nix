61a5fec0d4b10b3412de63cc405e1f58a5551bda
Chloé Kekoa <chloekek@use.startmail.com>
2022-04-06 19:30:30 +0200

    Upgrade Rust to latest version

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@
 
 let
     rust = nixpkgs.rustChannelOf {
-        date = "2022-02-24";
+        date = "2022-04-06";
         channel = "nightly";
-        sha256 = "13sawwb4yn3akj68s59ba1hrqi4lr59ihdis0mvbbcq4p934m4jf";
+        sha256 = "1jviyb0h3w0i963y38i63g1ddf0v1j4g8b3ijn96l0a518xb7qdw";
     };
 in
     nixpkgs.mkShell {
